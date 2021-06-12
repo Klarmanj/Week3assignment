6a61601d053bb3a5ab875772f3b8beeea59486e7
Klarmanj <72238062+Klarmanj@users.noreply.github.com>
2021-06-12 01:05:15 -0400

    Update week3assignment.cpp

--- week3assignment.cpp
+++ week3assignment.cpp
@@ -2,8 +2,9 @@
 #include <iomanip>
 
 using namespace std;
-void dotts()
-{
+
+void dotts() {
+
     cout << setw(50) << setfill('.') << "" << endl;
 }
 int main()
@@ -11,9 +12,15 @@ int main()
     dotts();
     cout << left << "ITCS 2530 - Programming Assignment for week #3" << endl;
     dotts();
-    cout << " ";
+    cout << " " << endl;
 
     cout << left << "Please enter the item name: (no spaces)" << endl;
-    cout << left << "Is the item fragile? (y=yes/n=no)";
+    cout << "Is the item fragile? (y=yes/n=no)" << endl;
+    cout << "Please enter your order total" << endl;
+    cout << "Please enter destination" << endl << endl;
 
+    cout << "Your item is" << endl;
+    cout << "Your shipping cost is" << endl;
+    cout << "You are shipping to" << endl;
+    cout << "Your total shipping costs are" << endl;
 }
